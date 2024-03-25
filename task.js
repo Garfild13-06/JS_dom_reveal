1bef02bd135d2b45ef1f37532bd9d8109f654b66
garfild <garfild13-06@yandex.com>
2024-03-25 09:35:02 +0300

    Доработка для множества элементов

--- task.js
+++ task.js
@@ -1,20 +1,22 @@
-const reveal = document.querySelector('.reveal')
+const reveals = document.querySelectorAll('.reveal')
 
-function isVisible(el_) {
-    const {top, bottom} = reveal.getBoundingClientRect()
-    if (bottom < 0) {
-        return false
+reveals.forEach((reveal) => {
+    function isVisible(el_) {
+        const {top, bottom} = reveal.getBoundingClientRect()
+        if (bottom < 0) {
+            return false
+        }
+        if (top > window.innerHeight) {
+            return false
+        }
+        return true
     }
-    if (top > window.innerHeight) {
-        return false
-    }
-    return true
-}
 
-document.addEventListener('scroll', (e) => {
-    if (isVisible(reveal)) {
-        reveal.classList.add('reveal_active')
-    } else {
-        reveal.classList.remove('reveal_active')
-    }
+    document.addEventListener('scroll', (e) => {
+        if (isVisible(reveal)) {
+            reveal.classList.add('reveal_active')
+        } else {
+            reveal.classList.remove('reveal_active')
+        }
+    })
 })
